2d171a73b2dde9b67ab396d244d40316a7e78ad9
Dawid Gąbka <dawid.gabka@grandparade.co.uk>
2022-10-27 10:35:24 +0200

    fix vim-tmux navigation

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -234,7 +234,18 @@ lvim.plugins = {
 		ft = { "rust", "rs" },
 	},
 	{
-		"christoomey/vim-tmux-navigator",
+		"alexghergh/nvim-tmux-navigation",
+		config = function()
+			require("nvim-tmux-navigation").setup({
+				disable_when_zoomed = true, -- defaults to false
+				keybindings = {
+					left = "<C-h>",
+					down = "<C-j>",
+					up = "<C-k>",
+					right = "<C-l>",
+				},
+			})
+		end,
 	},
 }
 
