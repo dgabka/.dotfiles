921236c0af1ce4c5a59daf6f756a180cfe15186b
Dawid Gąbka <gabka.daw@gmail.com>
2022-12-12 11:32:51 +0100

    cleanup lvim config

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -1,73 +1,26 @@
---[[
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
-
 -- general
-lvim.log.level = "warn"
-lvim.format_on_save = true
 vim.opt.timeoutlen = 50
 vim.opt.cursorline = false
 vim.opt.hidden = true
--- lvim.colorscheme = "tokyonight"
--- vim.g.tokyonight_style = "storm"
+vim.opt.rnu = true
+lvim.transparent_window = true
+lvim.log.level = "warn"
+lvim.format_on_save = false
 lvim.colorscheme = "catppuccin-macchiato"
--- to disable icons and use a minimalist setup, uncomment the following
--- lvim.use_icons = false
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
--- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-L>"] = "<cmd>BufferLineCycleNext<cr>"
 lvim.keys.normal_mode["<S-H>"] = "<cmd>BufferLineCyclePrev<cr>"
--- unmap a default keymapping
--- vim.keymap.del("n", "<C-Up>")
--- override a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
 
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
-
--- Use which-key to add extra bindings with the leader-key prefix
-lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
--- 	name = "+Trouble",
--- 	r = { "<cmd>Trouble lsp_references<cr>", "References" },
--- 	f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
--- 	d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
--- 	q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
--- 	l = { "<cmd>Trouble loclist<cr>", "LocationList" },
--- 	w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
--- }
-
--- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
+lvim.builtin.project.active = false
 lvim.lsp.diagnostics.virtual_text = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
@@ -89,47 +42,6 @@ lvim.builtin.treesitter.ensure_installed = {
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
--- generic LSP settings
-
--- -- make sure server will always be installed even if the server is in skipped_servers list
--- lvim.lsp.installer.setup.ensure_installed = {
---     "sumeko_lua",
---     "jsonls",
--- }
--- -- change UI setting of `LspInstallInfo`
--- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
--- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
--- lvim.lsp.installer.setup.ui.border = "rounded"
--- lvim.lsp.installer.setup.ui.keymaps = {
---     uninstall_server = "d",
---     toggle_server_expand = "o",
--- }
-
--- ---@usage disable automatic installation of servers
--- lvim.lsp.automatic_servers_installation = false
-
--- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
--- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
-
--- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
--- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
--- vim.tbl_map(function(server)
---   return server ~= "emmet_ls"
--- end, lvim.lsp.automatic_configuration.skipped_servers)
-
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
-
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
@@ -148,21 +60,7 @@ formatters.setup({
 })
 
 -- -- set additional linters
-local linters = require("lvim.lsp.null-ls.linters")
-linters.setup({
-	{
-		command = "shellcheck",
-		filetypes = {
-			"sh",
-			"shell",
-			"bash",
-		},
-		extra_args = { "--severity", "warning" },
-	},
-	{
-		command = "jsonlint",
-	},
-})
+-- local linters = require("lvim.lsp.null-ls.linters")
 
 -- set additional code actions
 -- local code_actions = require("lvim.lsp.null-ls.code_actions")
@@ -184,43 +82,43 @@ require("dap-vscode-js").setup({
 	adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
 })
 
-for _, language in ipairs({ "typescript", "javascript" }) do
-	require("dap").configurations[language] = {
-		{
-			type = "pwa-node",
-			request = "launch",
-			name = "Launch file",
-			program = "${file}",
-			cwd = "${workspaceFolder}",
-		},
-		{
-			type = "pwa-node",
-			request = "attach",
-			name = "Attach",
-			processId = require("dap.utils").pick_process,
-			cwd = "${workspaceFolder}",
-		},
+require("dap").configurations["javascript"] = {
+	{
+		type = "pwa-node",
+		request = "launch",
+		name = "Launch file",
+		program = "${file}",
+		cwd = "${workspaceFolder}",
+	},
+	{
+		type = "pwa-node",
+		request = "attach",
+		name = "Attach",
+		processId = require("dap.utils").pick_process,
+		cwd = "${workspaceFolder}",
+	},
+}
 
-		{
-			type = "pwa-node",
-			request = "launch",
-			name = "Launch Typescript file",
-			program = "${file}",
-			cwd = "${workspaceFolder}",
-			protocol = "inspector",
-			console = "integratedTerminal",
-			runtimeExecutable = "${workspaceFolder}/node_modules/.bin/ts-node",
-			resolveSourceMapLocations = {
-				"${workspaceFolder}/dist/**/*.js",
-				"${workspaceFolder}/**",
-				"!**/node_modules/**",
-			},
-			skipFiles = { "<node_internals>/**", "**/node_modules/**" },
-			port = 9229,
-			sourceMaps = true,
+require("dap").configurations["typescript"] = {
+	{
+		type = "pwa-node",
+		request = "launch",
+		name = "Launch Typescript file",
+		program = "${file}",
+		cwd = "${workspaceFolder}",
+		protocol = "inspector",
+		console = "integratedTerminal",
+		runtimeExecutable = "${workspaceFolder}/node_modules/.bin/ts-node",
+		resolveSourceMapLocations = {
+			"${workspaceFolder}/dist/**/*.js",
+			"${workspaceFolder}/**",
+			"!**/node_modules/**",
 		},
-	}
-end
+		skipFiles = { "<node_internals>/**", "**/node_modules/**" },
+		port = 9229,
+		sourceMaps = true,
+	},
+}
 
 -- Additional Plugins
 lvim.plugins = {
@@ -314,9 +212,6 @@ lvim.plugins = {
 	},
 	{
 		"mxsdev/nvim-dap-vscode-js",
-	},
-	{
-		"jose-elias-alvarez/typescript.nvim",
 	},
 }
 
