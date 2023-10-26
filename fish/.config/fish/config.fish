03a15c7085d333197c7b9de1016e4b24649d137c
Dawid Gąbka <gabka.daw@gmail.com>
2023-10-27 01:53:25 +0200

    add pnpm support

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,3 +9,14 @@ starship init fish | source
 eval (brew shellenv)
 
 source /usr/local/opt/asdf/libexec/asdf.fish
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
+
+# pnpm
+set -gx PNPM_HOME "/Users/dgabka/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
