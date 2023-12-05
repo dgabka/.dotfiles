02e1b010cc67f74170f8e3f60d69910c392d76cb
Dawid Gąbka <gabka.daw@gmail.com>
2023-12-06 00:44:43 +0100

    sync configs

---
+++ fish/.config/fish/conf.d/colors.fish
@@ -0,0 +1,32 @@
+# https://fishshell.com/docs/current/interactive.html#syntax-highlighting-variables
+set -U fish_color_normal              normal
+set -U fish_color_command             brmagenta
+# set -U fish_color_keyword             
+set -U fish_color_quote               yellow
+set -U fish_color_redirection         bryellow
+set -U fish_color_end                 brmagenta
+set -U fish_color_error               red
+set -U fish_color_param               bryellow
+set -U fish_color_valid_path          green
+# set -U fish_color_option
+set -U fish_color_comment             cyan
+set -U fish_color_selection           'white' '--background=#540099'
+set -U fish_color_operator            cyan
+set -U fish_color_escape              cyan
+set -U fish_color_autosuggestion      cyan
+set -U fish_color_cwd                 blue
+set -U fish_color_cwd_root            red
+set -U fish_color_user                brmagenta
+set -U fish_color_host                blue
+# set -U fish_color_host_remote         
+set -U fish_color_status              red
+set -U fish_color_cancel              -r
+set -U fish_color_search_match        -r
+
+set -U fish_color_history_current     --bold '--background=#540099'
+set -U fish_color_match               --background=lue
+set -U fish_pager_color_completion    normal
+set -U fish_pager_color_description   yellow
+set -U fish_pager_color_prefix        'white' '--bold' '--underline'
+set -U fish_pager_color_progress      'brwhite' '--background=cyan'
+
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# environment
+set fzf_fd_opts --hidden --exclude=.git
 set EDITOR hx
 
 starship init fish | source
@@ -11,7 +13,7 @@ set -gx PATH "/opt/homebrew/bin" $PATH
 eval (brew shellenv)
 
 # tabtab source for packages
-# uninstall by removing these lines
+# pnpm
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 
 # pnpm
