31c67b2fb9a5c2a44026a43b76c5b978e6f2eb89
Dawid Gąbka <gabka.daw@gmail.com>
2023-12-20 12:35:56 +0100

    update indentline to v3

--- nvim/.config/nvim/lua/config/indentline.lua
+++ nvim/.config/nvim/lua/config/indentline.lua
@@ -1,32 +1,23 @@
 local M = {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
   event = "VeryLazy",
-  commit = "9637670896b68805430e2f72cf5d16be5b97a22a",
 }
 
 function M.config()
-  local icons = require "config.icons"
-
-  require("indent_blankline").setup {
-    buftype_exclude = { "terminal", "nofile" },
-    filetype_exclude = {
-      "help",
-      "startify",
-      "dashboard",
-      "lazy",
-      "neogitstatus",
-      "NvimTree",
-      "Trouble",
-      "text",
+  require("ibl").setup {
+    exclude = {
+      filetypes = {
+        "help",
+        "lazy",
+        "oil",
+        "text",
+      },
     },
-    -- char = icons.ui.LineLeft,
-    char = icons.ui.LineMiddle,
-    -- context_char = icons.ui.LineLeft,
-    context_char = icons.ui.LineMiddle,
-    show_trailing_blankline_indent = false,
-    show_first_indent_level = true,
-    use_treesitter = true,
-    show_current_context = true,
+    scope = {
+      show_start = false,
+      show_end = false,
+    },
   }
 end
 
