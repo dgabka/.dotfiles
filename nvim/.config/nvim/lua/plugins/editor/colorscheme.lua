ab104332fb333d4ee29038bfafd1a7dc3c3da7a1
Dawid Gąbka <gabka.daw@gmail.com>
2024-03-06 01:08:52 +0100

    fix colorscheme config

--- nvim/.config/nvim/lua/plugins/editor/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/editor/colorscheme.lua
@@ -3,16 +3,30 @@ return {
     "rose-pine/neovim",
     name = "rose-pine",
     priority = 1000,
-    opts = {
-
-      variant = "moon",
-      disable_italics = false,
-
-      highlight_groups = {
-        Comment = { italic = true },
-      },
-    },
     config = function()
+      require("rose-pine").setup {
+
+        variant = "moon",
+        disable_italics = false,
+        dim_inactive_windows = true,
+
+        highlight_groups = {
+          Comment = { italic = true },
+
+          TelescopeBorder = { fg = "overlay", bg = "overlay" },
+          TelescopeNormal = { fg = "subtle", bg = "overlay" },
+          TelescopeSelection = { fg = "text", bg = "highlight_med" },
+          TelescopeSelectionCaret = { fg = "love", bg = "highlight_med" },
+          TelescopeMultiSelection = { fg = "text", bg = "highlight_high" },
+
+          TelescopeTitle = { fg = "base", bg = "love" },
+          TelescopePromptTitle = { fg = "base", bg = "pine" },
+          TelescopePreviewTitle = { fg = "base", bg = "iris" },
+
+          TelescopePromptNormal = { fg = "text", bg = "surface" },
+          TelescopePromptBorder = { fg = "surface", bg = "surface" },
+        },
+      }
       vim.cmd.colorscheme "rose-pine-moon"
     end,
   },
