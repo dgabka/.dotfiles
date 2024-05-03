fe3a40471b64a3442708213f37ee6ed5607521fc
Dawid Gąbka <gabka.daw@gmail.com>
2024-05-04 01:08:53 +0200

    edit tab plugin

--- nvim/.config/nvim/lua/plugins/editor/tabby.lua
+++ nvim/.config/nvim/lua/plugins/editor/tabby.lua
@@ -2,7 +2,7 @@ return {
   "nanozuki/tabby.nvim",
   event = "VimEnter",
   config = function()
-    require("tabby.tabline").use_preset("active_wins_at_tail", {
+    require("tabby.tabline").use_preset("active_wins_at_end", {
       tab_name = {
         name_fallback = function()
           return ""
@@ -11,6 +11,7 @@ return {
       buf_name = {
         mode = "unique",
       },
+      lualine_theme = "rose-pine-alt"
     })
   end,
 }
