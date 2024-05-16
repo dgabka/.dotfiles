c5f8d08191569772be52c94102e6c46ab36f5018
Dawid Gąbka <gabka.daw@gmail.com>
2024-05-17 01:45:17 +0200

    add fzf-rg search widget

--- nix-system/commons/zsh.nix
+++ nix-system/commons/zsh.nix
@@ -17,4 +17,20 @@
   syntaxHighlighting = {
     enable = true;
   };
+  initExtra = ''
+    function fzf-grep-widget {
+      RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
+      INITIAL_QUERY="$1"
+      : | fzf --ansi --disabled --query "$INITIAL_QUERY" \
+      --bind "start:reload:$RG_PREFIX {q}" \
+      --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
+      --delimiter : \
+      --preview 'bat --color=always {1} --highlight-line {2}' \
+      --preview-window '60%,+{2}+3/3,~3' \
+      --bind 'enter:become(nvim {1} +{2})' \
+      --height 100%
+    }
+    zle -N fzf-grep-widget
+    bindkey '^[^F' fzf-grep-widget
+  '';
 }
