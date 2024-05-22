e12ede96b20aa3d0678001319ffbab66839c0b65
Dawid Gąbka <gabka.daw@gmail.com>
2024-05-22 17:43:38 +0200

    open fugitive in full window

--- nvim/.config/nvim/lua/dgabka/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/dgabka/plugins/fugitive.lua
@@ -1,8 +1,9 @@
 return {
     "tpope/vim-fugitive",
     config = function()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-        vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-        vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+        vim.keymap.set("n", "<leader>gs", ":0G<CR>")
+        -- TODO: add when working on some conflict
+        -- vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+        -- vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
     end,
 }
