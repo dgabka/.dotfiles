9b0873986e1c5d1e2d2958f7e67ed8fa13e68018
Dawid Gąbka <gabka.daw@gmail.com>
2024-05-25 01:30:26 +0200

    de-cyanize code highlighting

--- nvim/.config/nvim/lua/dgabka/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/dgabka/plugins/colorscheme.lua
@@ -8,6 +8,16 @@ return {
                 variant = "main",
                 disable_italics = true,
                 dim_inactive_windows = true,
+
+                highlight_groups = {
+                    ["@lsp.type.property.nix"] = { fg = "text" },
+                    ["@lsp.type.property.lua"] = { fg = "text" },
+                    ["@variable.member.typescript"] = { fg = "text" },
+                    ["@type.builtin.typescript"] = { fg = "iris" },
+                    ["@lsp.type.parameter.typescript"] = { fg = "text" },
+                    ["@lsp.type.property.typescriptreact"] = { fg = "text" },
+                    ["@tag.tsx"] = { fg = "text" },
+                },
             }
             vim.cmd.colorscheme "rose-pine"
         end,
