d69e85ecbbb063ae8382eb37238c77674e57a84b
Dawid Gąbka <gabka.daw@gmail.com>
2024-05-25 01:43:12 +0200

    further de-cyanize colorscheme

--- nvim/.config/nvim/lua/dgabka/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/dgabka/plugins/colorscheme.lua
@@ -12,11 +12,17 @@ return {
                 highlight_groups = {
                     ["@lsp.type.property.nix"] = { fg = "text" },
                     ["@lsp.type.property.lua"] = { fg = "text" },
+                    ["@constructor.lua"] = { fg = "muted" },
                     ["@variable.member.typescript"] = { fg = "text" },
                     ["@type.builtin.typescript"] = { fg = "iris" },
                     ["@lsp.type.parameter.typescript"] = { fg = "text" },
+                    ["@lsp.type.property.typescript"] = { fg = "text" },
+                    ["@lsp.type.interface.typescript"] = { fg = "iris" },
+                    ["@function.method.call.typescript"] = { fg = "rose" },
                     ["@lsp.type.property.typescriptreact"] = { fg = "text" },
                     ["@tag.tsx"] = { fg = "text" },
+                    ["@type.toml"] = { fg = "iris" },
+                    ["@property.toml"] = { fg = "text" },
                 },
             }
             vim.cmd.colorscheme "rose-pine"
