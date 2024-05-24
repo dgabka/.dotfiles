844f9ac1ad98521e58cef7bf9f85e84aa932cc49
Dawid Gąbka <dawid.gabka@grandparade.co.uk>
2024-05-24 18:37:51 +0200

    remove unnecessary treesitter config

--- nvim/.config/nvim/lua/dgabka/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/dgabka/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
                 "javascript",
                 "typescript",
                 "lua",
-                "jsdoc",
+                "nix",
                 "bash",
             },
             sync_install = false,
@@ -16,20 +16,9 @@ return {
             indent = {
                 enable = true,
             },
-
             highlight = {
                 enable = true,
             },
         }
-
-        local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        treesitter_parser_config.templ = {
-            install_info = {
-                url = "https://github.com/vrischmann/tree-sitter-templ.git",
-                files = { "src/parser.c", "src/scanner.c" },
-                branch = "master",
-            },
-        }
-        vim.treesitter.language.register("templ", "templ")
     end,
 }
