306e0151a650428c59f9535881d480da20b84554
Dawid Gąbka <gabka.daw@gmail.com>
2024-05-26 23:13:54 +0200

    de-cyanize colorscheme

--- nvim/.config/nvim/lua/dgabka/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/dgabka/plugins/colorscheme.lua
@@ -2,6 +2,7 @@ return {
     {
         "rose-pine/neovim",
         name = "rose-pine",
+
         priority = 1000,
         config = function()
             require("rose-pine").setup {
@@ -10,19 +11,20 @@ return {
                 dim_inactive_windows = true,
 
                 highlight_groups = {
-                    ["@lsp.type.property.nix"] = { fg = "text" },
-                    ["@lsp.type.property.lua"] = { fg = "text" },
+                    Label = { fg = "pine" },
+
+                    ["@field"] = { fg = "text" },
+                    ["@variable.member"] = { fg = "text" },
+                    ["@property"] = { fg = "text", italic = false },
+
+                    ["@type"] = { fg = "iris" },
+                    ["@type.builtin"] = { fg = "iris", bold = true, italic = true },
+
                     ["@constructor.lua"] = { fg = "muted" },
-                    ["@variable.member.typescript"] = { fg = "text" },
-                    ["@type.builtin.typescript"] = { fg = "iris" },
-                    ["@lsp.type.parameter.typescript"] = { fg = "text" },
-                    ["@lsp.type.property.typescript"] = { fg = "text" },
-                    ["@lsp.type.interface.typescript"] = { fg = "iris" },
-                    ["@function.method.call.typescript"] = { fg = "rose" },
-                    ["@lsp.type.property.typescriptreact"] = { fg = "text" },
-                    ["@tag.tsx"] = { fg = "text" },
-                    ["@type.toml"] = { fg = "iris" },
-                    ["@property.toml"] = { fg = "text" },
+
+                    ["@markup.heading"] = { fg = "rose" },
+
+                    TelescopeTitle = { fg = "pine", bold = true },
                 },
             }
             vim.cmd.colorscheme "rose-pine"
