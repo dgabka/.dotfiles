36a2c2bb4cb072ec3c276a7f9432a7f29af49488
Dawid Gąbka <gabka.daw@gmail.com>
2024-05-27 00:45:37 +0200

    remove redundant ftplugin files for typescript

--- nvim/.config/nvim/after/ftplugin/typescriptreact.lua
+++
@@ -1,1 +0,0 @@
-vim.cmd.runtime { "after/ftplugin/typescript.lua", bang = true }
--- nvim/.config/nvim/lua/dgabka/plugins/typescript.lua
+++ nvim/.config/nvim/lua/dgabka/plugins/typescript.lua
@@ -4,7 +4,12 @@ return {
     ft = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
     lazy = true,
     config = function()
-        require("typescript-tools").setup {}
-        -- TODO: add keymaps for new functionality
+        require("typescript-tools").setup {
+            settings = { expose_as_code_action = { "add_missing_imports", "organize_imports" } },
+            jsx_close_tag = {
+                enable = false,
+                filetypes = { "javascriptreact", "typescriptreact" },
+            },
+        }
     end,
 }
