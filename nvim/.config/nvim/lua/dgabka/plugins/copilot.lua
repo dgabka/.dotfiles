6fbdb54911e44485b7abbad7c6fb0d206c7c6080
Dawid Gąbka <gabka.daw@gmail.com>
2024-11-09 14:45:15 +0100

    add copilot

---
+++ nvim/.config/nvim/lua/dgabka/plugins/copilot.lua
@@ -0,0 +1,8 @@
+return {
+    "github/copilot.vim",
+    config = function()
+        -- Optional configurations for Copilot
+        -- vim.g.copilot_no_tab_map = true -- Disable default <Tab> mapping
+        -- vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    end,
+}
