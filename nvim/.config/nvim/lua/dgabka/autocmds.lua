5af999d582e5a9c82180d6d590b05f384237a1c6
Dawid Gąbka <dawid.gabka@grandparade.co.uk>
2025-02-25 12:38:14 +0100

    remove xcode autocmd

--- nvim/.config/nvim/lua/dgabka/autocmds.lua
+++ nvim/.config/nvim/lua/dgabka/autocmds.lua
@@ -1,24 +1,3 @@
-vim.api.nvim_create_autocmd("User", {
-    pattern = { "XcodebuildBuildFinished", "XcodebuildTestsFinished" },
-    callback = function(event)
-        if event.data.cancelled then
-            return
-        end
-
-        if event.data.success then
-            require("trouble").close()
-        elseif not event.data.failedCount or event.data.failedCount > 0 then
-            if next(vim.fn.getqflist()) then
-                require("trouble").open "quickfix"
-            else
-                require("trouble").close()
-            end
-
-            require("trouble").refresh()
-        end
-    end,
-})
-
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave", "TextChanged" }, {
