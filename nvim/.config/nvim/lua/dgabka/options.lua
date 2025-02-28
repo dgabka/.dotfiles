8b0148e904aa8ac316e5514b7a14c3417780e2fe
Dawid Gąbka <dawid.gabka@grandparade.co.uk>
2025-02-28 02:22:58 +0100

    update lua settings

--- nvim/.config/nvim/lua/dgabka/options.lua
+++ nvim/.config/nvim/lua/dgabka/options.lua
@@ -10,13 +10,13 @@ vim.opt.expandtab = true -- convert tabs to spaces
 vim.opt.cursorline = true -- highlight the current line
 vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 2 -- insert 2 spaces for a tab
-vim.opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
-vim.opt.smartindent = true -- make indenting smarter again
+vim.opt.cmdheight = 1
+vim.opt.smartindent = true
 vim.opt.updatetime = 100
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.autoread = true -- reload files changed outside of vim
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = "menuone,noselect"
+vim.o.completeopt = "menu,menuone,noselect"
 vim.o.termguicolors = true
