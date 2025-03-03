3c5f0b1df96c29678c218465808b85cea7c02880
Dawid Gąbka <gabka.daw@gmail.com>
2025-03-03 03:11:19 +0100

    update typescript settings

--- nvim/.config/nvim/lua/dgabka/plugins/typescript.lua
+++ nvim/.config/nvim/lua/dgabka/plugins/typescript.lua
@@ -1,30 +1,33 @@
 return {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    ft = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-    lazy = true,
-    config = function()
-        local tt = require "typescript-tools"
+  "pmizio/typescript-tools.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  ft = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+  lazy = true,
+  config = function()
+    local tt = require "typescript-tools"
 
-        tt.setup {
-            code_lens = "all",
-            tsserver_file_preferences = {
-                includeInlayEnumMemberValueHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayParameterNameHints = "all",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                quotePreference = "auto",
-            },
-            jsx_close_tag = {
-                enable = true,
-                filetypes = { "javascriptreact", "typescriptreact" },
-            },
-        }
-
-        vim.keymap.set("n", "<leader>lo", "<cmd>TSToolsOrganizeImports<CR>", { desc = "Organize imports" })
-        vim.keymap.set("n", "<leader>li", "<cmd>TSToolsAddMissingImports<CR>", { desc = "Organize imports" })
-    end,
+    tt.setup {
+      settings = {
+        code_lens = "all",
+        tsserver_file_preferences = {
+          includeInlayEnumMemberValueHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayParameterNameHints = "all",
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeCompletionsForModuleExports = true,
+          includeAutomaticOptionalChainCompletions = true,
+          quotePreference = "single",
+        },
+        jsx_close_tag = {
+          enable = true,
+          filetypes = { "javascriptreact", "typescriptreact" },
+        },
+      },
+    }
+    vim.keymap.set("n", "<leader>lo", "<cmd>TSToolsOrganizeImports<CR>", { desc = "Organize imports" })
+    vim.keymap.set("n", "<leader>li", "<cmd>TSToolsAddMissingImports<CR>", { desc = "Organize imports" })
+  end,
 }
