1082f36345b3d75e9d36c9b12873fb8df97758a8
Dawid Gąbka <gabka.daw@gmail.com>
2025-03-03 03:27:21 +0100

    update keymaps and options

--- nvim/.config/nvim/lua/dgabka/keymaps.lua
+++ nvim/.config/nvim/lua/dgabka/keymaps.lua
@@ -1,22 +1,46 @@
+local set = vim.keymap.set
+
 -- Ctrl-C to exit insert mode
-vim.keymap.set("i", "<C-c>", "<Esc>")
+set("i", "<C-c>", "<Esc>")
 
 -- open oil
-vim.keymap.set("n", "<leader>-", ":Oil<CR>")
+set("n", "<leader>-", ":Oil<CR>")
 
 -- quickfix list
-vim.keymap.set("n", "<leader>co", ":copen<CR>", { desc = "Open" })
-vim.keymap.set("n", "<leader>cn", ":cnext<CR>", { desc = "Next" })
-vim.keymap.set("n", "<leader>cp", ":cprev<CR>", { desc = "Prev" })
-vim.keymap.set("n", "<leader>cc", ":cclose<CR>", { desc = "Close" })
+set("n", "<leader>co", ":copen<CR>", { desc = "Open" })
+set("n", "<leader>cn", ":cnext<CR>", { desc = "Next" })
+set("n", "<leader>cp", ":cprev<CR>", { desc = "Prev" })
+set("n", "<leader>cc", ":cclose<CR>", { desc = "Close" })
 
 -- move visual selection up/down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+set("v", "J", ":m '>+1<CR>gv=gv")
+set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- replace paste without overwriting register
-vim.keymap.set("x", "<leader>p", [["_dP]])
+set("x", "<leader>p", [["_dP]])
 
 -- yank to system clipboard
-vim.keymap.set("v", "<leader>y", [["+y]], { desc = "Yank to clipboard" })
-vim.keymap.set("n", "<leader>y", [["+Y]], { desc = "Yank line to clipboard" })
+set("v", "<leader>y", [["+y]], { desc = "Yank to clipboard" })
+set("n", "<leader>y", [["+Y]], { desc = "Yank line to clipboard" })
+
+-- toggle inlay_hints
+set("n", "<space>tt", function()
+  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = 0 }, { bufnr = 0 })
+end)
+
+-- These mappings control the size of splits (height/width)
+set("n", "<M-,>", "<c-w>5<")
+set("n", "<M-.>", "<c-w>5>")
+set("n", "<M-t>", "<C-W>+")
+set("n", "<M-s>", "<C-W>-")
+
+-- Disable hlsearch if it's on, otherwise just do "enter"
+set("n", "<CR>", function()
+  ---@diagnostic disable-next-line: undefined-field
+  if vim.v.hlsearch == 1 then
+    vim.cmd.nohl()
+    return ""
+  else
+    return vim.keycode "<CR>"
+  end
+end, { expr = true })
--- nvim/.config/nvim/lua/dgabka/options.lua
+++ nvim/.config/nvim/lua/dgabka/options.lua
@@ -10,12 +10,13 @@ vim.opt.expandtab = true -- convert tabs to spaces
 vim.opt.cursorline = true -- highlight the current line
 vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 2 -- insert 2 spaces for a tab
-vim.opt.cmdheight = 1
 vim.opt.smartindent = true
 vim.opt.updatetime = 100
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.autoread = true -- reload files changed outside of vim
+vim.opt.formatoptions:remove "o" -- Don't have `o` add a comment
+vim.opt.inccommand = "split"
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = "menu,menuone,noselect"
